c0121d4817144618df50c4fb32bf656f136da028
vmaxtacular513 <veronicamaxwell152@gmail.com>
2020-11-28 17:39:57 -0500

    added folder structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-
+import React from 'react';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello App </h1>
-    </div>
-  );
+class App extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <h1>It still works!</h1>
+     );
+  }
 }
-
+ 
 export default App;
