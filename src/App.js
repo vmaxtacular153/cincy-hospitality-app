026999971f5b18cbba44e9d1e122294779c45e06
vmaxtacular513 <veronicamaxwell152@gmail.com>
2020-11-28 21:14:11 -0500

    added underversal styling for font-family to App.scss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 import MainButton from './elements/button.element';
 
@@ -9,6 +8,8 @@ class App extends Component {
   render() { 
     return ( 
       <div>
+        <h1>Heading Font</h1>
+        <p>paragraph font</p>
         <MainButton />
         <MainButton />
       </div>
